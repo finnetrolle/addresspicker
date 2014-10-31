db0ffac801c3c94a60eb798d8353384e66df3ab8
Stepan <egorov_s@atr-sz.ru>
2014-10-31 18:35:27 +0300

    LENGISTP-37

--- AddressPicker/AddressPickerSettings.js
+++ AddressPicker/AddressPickerSettings.js
@@ -10,7 +10,7 @@ define([
         appinfo: {
             major: 0,   // application version
             minor: 3,   // first were prototype
-            build: 7,  // number of commit
+            build: 8,  // number of commit
             beta: true, // beta version
             demo: true  // demonstartion version
         },
