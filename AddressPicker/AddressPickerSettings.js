6c5b041dfa57e5c9b039a7679712e2a88ce2d76d
Stepan <egorov_s@atr-sz.ru>
2014-11-05 12:29:26 +0300

    LENGISTP-33 клик по стрелочке выбора подложки

--- AddressPicker/AddressPickerSettings.js
+++ AddressPicker/AddressPickerSettings.js
@@ -10,7 +10,7 @@ define([
         appinfo: {
             major: 0,   // application version
             minor: 3,   // first were prototype
-            build: 11,  // number of commit
+            build: 12,  // number of commit
             beta: true, // beta version
             demo: true  // demonstartion version
         },
