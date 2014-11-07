36f8d696a30c3ccd0d28df2c61fe6d4a048bbf90
syachin <maxsyachin@gmail.com>
2014-11-07 21:04:10 +0300

    0.3.21 Ready to insert

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+// Обязательная обёртка
+module.exports = function(grunt) {
+
+    // Задачи
+    grunt.initConfig({
+        // Склеиваем
+        concat: {
+            main: {
+                src: [
+                    'AddressPicker/**/*.js'
+                ],
+                dest: 'AddressPicker.js'
+            }
+        },
+        // Сжимаем
+        uglify: {
+            main: {
+                files: {
+                    // Результат задачи concat
+                    'AddressPicker.min.js': '<%= concat.main.dest %>'
+                }
+            }
+        }
+    });
+
+    // Загрузка плагинов, установленных с помощью npm install
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Задача по умолчанию
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
