51202bf5a6cf7dab1cfb0562a290e7feb6851d13
Donat123 <Donat123>
2014-12-17 18:38:01 +0300

    0.3.37 Refactoring css file

--- AddressPicker/AddressPickerSettings.js
+++ AddressPicker/AddressPickerSettings.js
@@ -10,7 +10,7 @@ define([
         appinfo: {
             major: 0,   // application version
             minor: 3,   // first were prototype
-            build: 36,  // number of commit
+            build: 37,  // number of commit
             beta: true, // beta version
             demo: true, // demonstartion version
             developer: 'ООО ИГИТ',
