fc44e434890b01b4d7f092b528f50a458373a21c
Max Syachin <maxsyachin@gmail.com>
2014-12-17 12:20:23 +0300

    0.3.35 updated version (build to 35)

--- AddressPicker/AddressPickerSettings.js
+++ AddressPicker/AddressPickerSettings.js
@@ -10,7 +10,7 @@ define([
         appinfo: {
             major: 0,   // application version
             minor: 3,   // first were prototype
-            build: 34,  // number of commit
+            build: 35,  // number of commit
             beta: true, // beta version
             demo: true, // demonstartion version
             developer: 'ООО ИГИТ',
